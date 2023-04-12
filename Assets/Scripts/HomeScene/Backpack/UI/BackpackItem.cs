d0d0ead24f8bcd536855e7111fc656959f9ad64f
ken5170696 <96158757+ken5170696@users.noreply.github.com>
2023-04-12 20:17:58 +0800

    inventory and item added

--- Assets/Scripts/HomeScene/Backpack/UI/BackpackItem.cs
+++ Assets/Scripts/HomeScene/Backpack/UI/BackpackItem.cs
@@ -7,78 +7,83 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-// reference: https://github.com/SunnyValleyStudio/Unity-Inventory-system-using-SO-and-MVC
-public class BackpackItem : MonoBehaviour, IPointerClickHandler,
-        IBeginDragHandler, IEndDragHandler, IDropHandler, IDragHandler
+namespace Inventory.UI
 {
-    [SerializeField] Image borderImage;
-    [SerializeField] Image itemImage;
-    [SerializeField] TMP_Text quantityText;
-
-    public event Action<BackpackItem> OnItemClicked, OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag, OnItemRightMouseBtnClicked;
-
-
-    private bool empty = true;
-
-    public void Awake()
+    // reference: https://github.com/SunnyValleyStudio/Unity-Inventory-system-using-SO-and-MVC
+    public class BackpackItem : MonoBehaviour, IPointerClickHandler,
+            IBeginDragHandler, IEndDragHandler, IDropHandler, IDragHandler
     {
+        [SerializeField] Image borderImage;
+        [SerializeField] Image itemImage;
+        [SerializeField] TMP_Text quantityText;
 
-    }
+        public event Action<BackpackItem> OnItemClicked, OnItemDroppedOn, OnItemBeginDrag, OnItemEndDrag, OnItemRightMouseBtnClicked;
 
-    public void ResetData()
-    {
 
-    }
-    public void Deselect()
-    {
-        borderImage.enabled = false;
-    }
+        private bool empty = true;
 
-    public void Select()
-    {
-        borderImage.enabled = true;
-    }
-
-    public void SetData(Sprite sprite, int quantity)
-    {
-        itemImage.gameObject.SetActive(true);
-        itemImage.sprite = sprite;
-        quantityText.text = quantity.ToString();
-        empty = false;
-    }
-
-    #region Event Handler
-    public void OnDrag(PointerEventData eventData)
-    {
-
-    }
-
-    public void OnDrop(PointerEventData eventData)
-    {
-        OnItemDroppedOn?.Invoke(this);
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        OnItemEndDrag?.Invoke(this);
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        if (empty) { return; }
-        OnItemBeginDrag?.Invoke(this);
-    }
-
-    public void OnPointerClick(PointerEventData pointerEventData)
-    {
-        if (pointerEventData.button == PointerEventData.InputButton.Right)
+        public void Awake()
         {
-            OnItemRightMouseBtnClicked?.Invoke(this);
+
         }
-        else
+
+        public void ResetData()
         {
-            OnItemClicked?.Invoke(this);
+            itemImage.gameObject.SetActive(false);
+            empty = true;
         }
+        public void Deselect()
+        {
+            borderImage.enabled = false;
+        }
+
+        public void Select()
+        {
+            borderImage.enabled = true;
+        }
+
+        public void SetData(Sprite sprite, int quantity)
+        {
+            itemImage.gameObject.SetActive(true);
+            itemImage.sprite = sprite;
+            quantityText.text = quantity.ToString();
+            empty = false;
+        }
+
+        #region Event Handler
+        public void OnDrag(PointerEventData eventData)
+        {
+
+        }
+
+        public void OnDrop(PointerEventData eventData)
+        {
+            OnItemDroppedOn?.Invoke(this);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            OnItemEndDrag?.Invoke(this);
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            if (empty) { return; }
+            OnItemBeginDrag?.Invoke(this);
+        }
+
+        public void OnPointerClick(PointerEventData pointerEventData)
+        {
+            if (pointerEventData.button == PointerEventData.InputButton.Right)
+            {
+                OnItemRightMouseBtnClicked?.Invoke(this);
+            }
+            else
+            {
+                OnItemClicked?.Invoke(this);
+            }
+        }
+        #endregion
     }
-    #endregion
+
 }
