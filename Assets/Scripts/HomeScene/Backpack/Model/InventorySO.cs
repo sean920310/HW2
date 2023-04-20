f17ca083019c66ba49630df42160711c159e1ee0
ken5170696 <96158757+ken5170696@users.noreply.github.com>
2023-04-20 14:19:03 +0800

    INVENTORY NOT YET

--- Assets/Scripts/HomeScene/Backpack/Model/InventorySO.cs
+++ Assets/Scripts/HomeScene/Backpack/Model/InventorySO.cs
@@ -25,7 +25,7 @@ namespace Inventory.Model
                 inventoryItems.Add(InventoryItem.GetEmptyItem());
             }
         }
-        public int AddItem(ItemSO item, int quantity)
+        public int AddItem(ItemSO item, int quantity, List<ItemParameter> itemState = null)
         {
             if (item.IsStackable == false)
             {
@@ -33,7 +33,7 @@ namespace Inventory.Model
                 {
                     while (quantity > 0 && IsInventoryFull() == false)
                     {
-                        quantity -= AddNonStackableItem(item, 1);
+                        quantity -= AddItemToFirstFreeSlot(item, 1, itemState);
                     }
                     InformAboutChange();
                     return quantity;
@@ -43,6 +43,7 @@ namespace Inventory.Model
             InformAboutChange();
             return quantity;
         }
+
 
         private int AddNonStackableItem(ItemSO item, int quantity)
         {
@@ -62,12 +63,15 @@ namespace Inventory.Model
             return 0;
         }
 
-        private int AddItemToFirstFreeSlot(ItemSO item, int quantity)
+        private int AddItemToFirstFreeSlot(ItemSO item, int quantity
+           , List<ItemParameter> itemState = null)
         {
             InventoryItem newItem = new InventoryItem
             {
                 item = item,
-                quantity = quantity
+                quantity = quantity,
+                itemState =
+                new List<ItemParameter>(itemState == null ? item.DefaultParametersList : itemState)
             };
 
             for (int i = 0; i < inventoryItems.Count; i++)
@@ -80,6 +84,7 @@ namespace Inventory.Model
             }
             return 0;
         }
+
 
         private bool IsInventoryFull()
         => inventoryItems.Where(item => item.IsEmpty).Any() == false;
@@ -180,6 +185,7 @@ namespace Inventory.Model
     {
         public int quantity;
         public ItemSO item;
+        public List<ItemParameter> itemState;
         public bool IsEmpty => item == null;
 
         public InventoryItem ChangeQuantity(int newQuantity)
@@ -188,6 +194,7 @@ namespace Inventory.Model
             {
                 item = this.item,
                 quantity = newQuantity,
+                itemState = new List<ItemParameter>(this.itemState)
             };
         }
 
@@ -196,6 +203,7 @@ namespace Inventory.Model
             {
                 item = null,
                 quantity = 0,
+                itemState = new List<ItemParameter>()
             };
     }
 
